3cfef98b24557d9d3d4a1a434478aaf2e3c40908
ZyenxD <neyc30@gmail.com>
2016-04-12 23:32:03 -0400

    anadiendo sprites, seteando fisica en el ambiente, y demas detalles

--- src/resource.js
+++ src/resource.js
@@ -1,8 +1,14 @@
 var res = {
     HelloWorld_png : "res/HelloWorld.png",
+    red: "res/Angry_Bird_red.png",
+    background: "res/Angry_birds_background_by_gsgill37-d3kogmx.png",
+    pig: "res/Minion_pig_copy.png",
+    box:"res/box.png"
+    
 };
 
 var g_resources = [];
 for (var i in res) {
     g_resources.push(res[i]);
 }
+
